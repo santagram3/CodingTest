c05a5e6a2de8d456bc750e6439bbdb174bb24fdc
santagram3 <soslimso@naver.com>
2022-10-13 23:19:32 +0900

    중간

---
+++ src/Programers/LV0/PyungGyun9HaGi.java
@@ -0,0 +1,22 @@
+package Programers.LV0;
+
+public class PyungGyun9HaGi {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+	public static double solution(int[] arr) {
+	    double average = 0;
+
+	    	for (int i = 0; i < arr.length; i++) {
+			
+			}
+	    	return  0.0; 
+	    }
+	
+	
+	
+
+}
